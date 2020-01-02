4a82a8e447953994ffdf73f5aed6e203567103d8
songqi <308333575@163.com>
2020-01-02 09:41:35 +0800

    20200102

--- _book/gitalk-config.js
+++ _book/gitalk-config.js
@@ -1,10 +1,11 @@
+
 var gitalk = new Gitalk({
   "clientID": "100cc43a26a6aefa3006",
   "clientSecret": "3f8bbe3e213470d3d1620dfdd728a8a432cfee90",
   "repo": "weibiaodan",
   "owner": "a3664938",
   "admin": ["a3664938"],
-  "id": window.location.pathname,
+  "id": md5(window.location.pathname),
   "distractionFreeMode": false
 });
 gitalk.render("gitalk-container");
